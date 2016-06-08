b55e419407b7c09e2478299a875e2b97ff8f05ea
Kadłubek <179640@edu.p.lodz.pl>
2016-06-08 21:41:31 +0200

    test 3 failed

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -36,6 +36,18 @@ public class ServerLoadBalancerTest {
 		assertThat(theServer, hasCurrentLoadOf(100.0d));
 		assertThat("server should contain the vm", theServer.contains(theVm));
 	}
+	
+	@Test
+	public void balancingOneServerWithTenSlotsCapacity_andOneSlotVm_fillsTheServerWithTenPercent(){
+		Server theServer = a(server().withCapacity(10));
+		
+		Vm theVm = a(vm().ofSize(1));
+		
+		balancing(aServersListWith(theServer), aVmsListWith(theVm));
+		
+		assertThat(theServer, hasCurrentLoadOf(10.0d));
+		assertThat("server should contain the vm", theServer.contains(theVm));
+	}
 
 
 
